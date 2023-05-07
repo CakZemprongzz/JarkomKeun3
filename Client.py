69dfd0fd2670aef96f82634d944e84f71a6cb59f
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2023-05-07 12:45:02 +0700

    Changes to be committed:     new file:   Client.py 	new file:   Server.py
    Initial Start

---
+++ Client.py
@@ -0,0 +1,30 @@
+from socket import *
+
+serverName = 'localhost'
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+
+filename = input('Enter the name of the file you want to retrieve: ')
+
+request = f"GET /{filename} HTTP/1.1\r\nHost: {serverName}\r\n\r\n"
+clientSocket.send(request.encode())
+
+response = clientSocket.recv(1024)
+
+# Print the HTTP response message
+print(response.decode())
+
+# Print the content of the requested file, if available
+if response.decode().startswith('HTTP/1.1 200 OK'):
+    filedata = b''
+    while True:
+        data = clientSocket.recv(1024)
+        if not data:
+            break
+        filedata += data
+    print(filedata.decode())
+
+clientSocket.close()
+
