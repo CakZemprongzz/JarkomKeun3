c9ad59c45cfed84f362d8168d138487153bbd4f3
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2023-05-14 15:34:38 +0700

    Added Upload and Choose function  - Added new send to the server function (still broken)  - Add an option for the user to repeat and usend/retrieve from the server (the option is case insensitive)

--- Server/Server.py
+++ Server/Server.py
@@ -12,27 +12,50 @@ serverSocket.listen(1)
 print('Server Port is at :',serverPort)
 
 while True:
-    print('Ready to serve...') #Establish the connection
+    print('Ready to serve... waiting for respond') #Establish the connection
     connectionSocket, addr = serverSocket.accept()
-    try:
+    status = connectionSocket.recv(1024)
+
+    if status.decode().startswith('retrieve'): #Send to the client
+        print('Ready to send to the client!')
+        try:
+            message = connectionSocket.recv(1024).decode()
+            filename = message.split()[1]
+            print(filename)
+            with open(filename[1:], 'rb') as f:
+                outputdata = f.read()
+            print('Output data length:', len(outputdata))
+            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+            outputdataString = str(len(outputdata))
+            connectionSocket.send(outputdataString.encode())    
+            content_type, encoding = mimetypes.guess_type(filename)
+            print('Content-Type:', content_type)
+            connectionSocket.sendall(outputdata)
+            connectionSocket.close()
+        except IOError:     #Send response message if the file not found
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.send("File not found".encode())
+            connectionSocket.close() #Close client socket
+
+    elif status.decode().startswith('send'): #Retrieve from the client
+        print('Ready to retrieve')
         message = connectionSocket.recv(1024).decode()
-        filename = message.split()[1]
-        with open(filename[1:], 'rb') as f:
-            outputdata = f.read()
-        print('Output data length:', len(outputdata))
-        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
-        outputdataString = str(len(outputdata))
-        connectionSocket.send(outputdataString.encode())    
-        content_type, encoding = mimetypes.guess_type(filename)
-        print('Content-Type:', content_type)
-        connectionSocket.sendall(outputdata)
+        filename = message.split()[1] 
+        print(filename)
+        request = connectionSocket.recv(1024)
+        print(request.decode())
+        if request.decode().startswith('HTTP/1.1 200 OK'): # Save the content of the requested file to a local file
+            responseData = connectionSocket.recv(1024)
+            with open(filename, 'wb') as f:
+                while True:
+                    data = connectionSocket.recv(int(responseData.decode()))
+                    if not data:
+                        break
+                    f.write(data)
+            print('File saved to',filename)
         connectionSocket.close()
-    except IOError:     #Send response message if the file not found
-        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.send("File not found".encode())
-        connectionSocket.close() #Close client socket
- 
+        
 serverSocket.close()
 sys.exit() #Terminate the program sending the corresponding data
 
