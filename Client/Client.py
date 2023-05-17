7d5d26c254d6d171fb94fd3c7c552c7884e753d4
Ridla Aliya <ridlaaliyaa@student.telkomuniversity.ac.id>
2023-05-17 16:40:41 +0700

    added comment

--- Client/Client.py
+++ Client/Client.py
@@ -1,56 +1,60 @@
-from socket import *
+
+from socket import *  # Import the socket module
 import sys
 import mimetypes
 
-while True :
-    serverName = 'localhost' #input('Enter the IP : ')
-    serverPort = 12000 #int(input('Enter the Port : '))
+serverSocket = socket(AF_INET, SOCK_STREAM)  # Create a server socket
 
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName,serverPort))
+serverPort = 12000  # Prepare a server socket
+serverSocket.bind(('', serverPort))  # Associate the server socket with the specified port and the localhost address
+serverSocket.listen(1)  # Start listening for incoming client connections (queue up to 1 client)
 
-    status = input('Retrieve file from server or Send file to the server (Retrieve/Send) : ').lower()
-    clientSocket.send(status.encode())
+print('Server Port is at:', serverPort)  # Print the server port
 
-    if status == 'retrieve': #Retrieve from the server
-        filename = input('Enter the name of the file you want to retrieve: ')
-        request = f"GET /{filename} HTTP/1.1\r\nHost: {serverName}\r\n\r\n"
-        clientSocket.send(request.encode())
-        response = clientSocket.recv(1024)
-        print(response.decode()) # Print the HTTP response message
-        if response.decode().startswith('HTTP/1.1 200 OK'): # Save the content of the requested file to a local file
-            responseData = clientSocket.recv(1024)
-            with open(filename, 'wb') as f:
-                while True:
-                    data = clientSocket.recv(int(responseData.decode()))
-                    if not data:
+while True:
+    print('Ready to serve... waiting for response')  # Wait for incoming connections
+    connectionSocket, addr = serverSocket.accept()  # Accept a connection
+    status = connectionSocket.recv(1024)  # Receive the status from the client
+
+    if status.decode().startswith('retrieve'):  # If status is "retrieve"
+        print('Ready to send to the client!')
+        try:
+            message = connectionSocket.recv(1024).decode()  # Receive the message from the client
+            filename = message.split()[1]  # Extract the filename from the message
+            print(filename)
+            with open(filename[1:], 'rb') as f:  # Open the file in binary mode for reading
+                outputdata = f.read()  # Read the content of the file
+            print('Output data length:', len(outputdata))  # Print the length of the output data
+            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())  # Send an OK response to the client
+            outputdataString = str(len(outputdata))  # Convert the length of the output data to a string
+            connectionSocket.send(outputdataString.encode())  # Send the length of the output data to the client
+            content_type, encoding = mimetypes.guess_type(filename)  # Guess the content type and encoding
+            print('Content-Type:', content_type)  # Print the content type of the file
+            connectionSocket.sendall(outputdata)  # Send the file data to the client
+            connectionSocket.close()  # Close the connection socket
+        except IOError:  # If the file is not found
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())  # Send a 404 Not Found response to the client
+            connectionSocket.send("\r\n".encode())  # Send an empty line as part of the response
+            connectionSocket.send("File not found".encode())  # Send a message indicating file not found
+            connectionSocket.close()  # Close the connection socket
+
+    elif status.decode().startswith('send'):  # If status is "send"
+        print('Ready to retrieve')
+        message = connectionSocket.recv(1024).decode()  # Receive the message from the client
+        filename = message.split()[1]  # Extract the filename from the message
+        print(filename)
+        request = connectionSocket.recv(1024)  # Receive the request from the client
+        print(request.decode())
+        if request.decode().startswith('HTTP/1.1 200 OK'):  # If the request is successful
+            responseData = connectionSocket.recv(1024)  # Receive the file data from the client
+            with open(filename, 'wb') as f:  # Open a file to save the received data
+                while True:  # Loop to receive and write the file data
+                    data = connectionSocket.recv(int(responseData.decode()))  # Receive data of specified length
+                    if not data:  # If no more data is received, break the loop
                         break
-                    f.write(data)
-            print('File saved to',filename)
-        clientSocket.close()
+                    f.write(data)  # Write the received data to the file
+            print('File saved to', filename)  # Saving file
+        connectionSocket.close()  # Close the connection socket
 
-    elif status == 'send': #Uploading to the server
-        try: 
-            filename = input('Enter the name of the file you want to send: ')
-            filename = '/' + filename
-            with open(filename[1:], 'rb') as f:
-                outputdata = f.read()
-            message = f"POST {filename} HTTP/1.1\r\nHost: {serverName}\r\nContent-Length: {outputdata}\r\n\r\n"
-            clientSocket.send(message.encode())
-            print('Output data length:', len(outputdata))
-            clientSocket.send("HTTP/1.1 200 OK\r\n".encode())
-            outputdataString = str(len(outputdata))
-            clientSocket.send(outputdataString.encode())    
-            content_type, encoding = mimetypes.guess_type(filename)
-            print('Content-Type:', content_type)
-            clientSocket.sendall(outputdata)
-            clientSocket.close()
-        except IOError:     #Send response message if the file not found
-            clientSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
-            clientSocket.send("\r\n".encode())
-            clientSocket.send("File not found".encode())
-            clientSocket.close() #Close client socket
-
-    choice = input('\nDo you want to retrieve another file? (yes/no): ')
-    if choice.lower() == 'no':
-        break
+serverSocket.close()  # Close the server socket
+sys.exit()  # Terminate the program sending the corresponding data
--- Server/Server.py
+++ Server/Server.py
@@ -1,61 +1,60 @@
 
-from socket import * #import socket module
-import sys 
+from socket import *  # Import the socket module
+import sys
 import mimetypes
 
-serverSocket = socket(AF_INET, SOCK_STREAM) # In order to terminate the program
+serverSocket = socket(AF_INET, SOCK_STREAM)  # Create a server socket
 
-serverPort = 12000 #Prepare a server socket
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
+serverPort = 12000  # Prepare a server socket
+serverSocket.bind(('', serverPort))  # Associate the server socket with the specified port and the localhost address
+serverSocket.listen(1)  # Start listening for incoming client connections (queue up to 1 client)
 
-print('Server Port is at :',serverPort)
+print('Server Port is at:', serverPort)  # Print the server port
 
 while True:
-    print('Ready to serve... waiting for respond') #Establish the connection
-    connectionSocket, addr = serverSocket.accept()
-    status = connectionSocket.recv(1024)
+    print('Ready to serve... waiting for response')  # Wait for incoming connections
+    connectionSocket, addr = serverSocket.accept()  # Accept a connection
+    status = connectionSocket.recv(1024)  # Receive the status from the client
 
-    if status.decode().startswith('retrieve'): #Send to the client
+    if status.decode().startswith('retrieve'):  # If status is "retrieve"
         print('Ready to send to the client!')
         try:
-            message = connectionSocket.recv(1024).decode()
-            filename = message.split()[1]
+            message = connectionSocket.recv(1024).decode()  # Receive the message from the client
+            filename = message.split()[1]  # Extract the filename from the message
             print(filename)
-            with open(filename[1:], 'rb') as f:
-                outputdata = f.read()
-            print('Output data length:', len(outputdata))
-            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
-            outputdataString = str(len(outputdata))
-            connectionSocket.send(outputdataString.encode())    
-            content_type, encoding = mimetypes.guess_type(filename)
-            print('Content-Type:', content_type)
-            connectionSocket.sendall(outputdata)
-            connectionSocket.close()
-        except IOError:     #Send response message if the file not found
-            connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
-            connectionSocket.send("\r\n".encode())
-            connectionSocket.send("File not found".encode())
-            connectionSocket.close() #Close client socket
+            with open(filename[1:], 'rb') as f:  # Open the file in binary mode for reading
+                outputdata = f.read()  # Read the content of the file
+            print('Output data length:', len(outputdata))  # Print the length of the output data
+            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())  # Send an OK response to the client
+            outputdataString = str(len(outputdata))  # Convert the length of the output data to a string
+            connectionSocket.send(outputdataString.encode())  # Send the length of the output data to the client
+            content_type, encoding = mimetypes.guess_type(filename)  # Guess the content type and encoding
+            print('Content-Type:', content_type)  # Print the content type of the file
+            connectionSocket.sendall(outputdata)  # Send the file data to the client
+            connectionSocket.close()  # Close the connection socket
+        except IOError:  # If the file is not found
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())  # Send a 404 Not Found response to the client
+            connectionSocket.send("\r\n".encode())  # Send an empty line as part of the response
+            connectionSocket.send("File not found".encode())  # Send a message indicating file not found
+            connectionSocket.close()  # Close the connection socket
 
-    elif status.decode().startswith('send'): #Retrieve from the client
+    elif status.decode().startswith('send'):  # If status is "send"
         print('Ready to retrieve')
-        message = connectionSocket.recv(1024).decode()
-        filename = message.split()[1] 
+        message = connectionSocket.recv(1024).decode()  # Receive the message from the client
+        filename = message.split()[1]  # Extract the filename from the message
         print(filename)
-        request = connectionSocket.recv(1024)
+        request = connectionSocket.recv(1024)  # Receive the request from the client
         print(request.decode())
-        if request.decode().startswith('HTTP/1.1 200 OK'): # Save the content of the requested file to a local file
-            responseData = connectionSocket.recv(1024)
-            with open(filename, 'wb') as f:
-                while True:
-                    data = connectionSocket.recv(int(responseData.decode()))
-                    if not data:
+        if request.decode().startswith('HTTP/1.1 200 OK'):  # If the request is successful
+            responseData = connectionSocket.recv(1024)  # Receive the file data from the client
+            with open(filename, 'wb') as f:  # Open a file to save the received data
+                while True:  # Loop to receive and write the file data
+                    data = connectionSocket.recv(int(responseData.decode()))  # Receive data of specified length
+                    if not data:  # If no more data is received, break the loop
                         break
-                    f.write(data)
-            print('File saved to',filename)
-        connectionSocket.close()
-        
-serverSocket.close()
-sys.exit() #Terminate the program sending the corresponding data
+                    f.write(data)  # Write the received data to the file
+            print('File saved to', filename)  # Saving file
+        connectionSocket.close()  # Close the connection socket
 
+serverSocket.close()  # Close the server socket
+sys.exit()  # Terminate the program sending the corresponding data
