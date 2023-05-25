70ba93868d50321563f46265a4fa2383e17b5b1c
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2023-05-25 21:18:15 +0700

    Adding comments

--- Client/Client.py
+++ Client/Client.py
@@ -12,7 +12,7 @@ while True:  # Start an infinite loop
     clientSocket.send(status.encode())  # Send the chosen option to the server
 
     if status == 'retrieve':  # If retrieving a file from the server
-        print('\nThe maximum is 100MB !')
+        print('\nThe maximum is 100MB !') #Inform the user about the limit
         filename = input('Enter the name of the file you want to retrieve: ')  # Input the file name to retrieve
         request = f"GET /{filename} HTTP/1.1\r\nHost: {serverName}\r\n\r\n"  # Create the HTTP GET request
 
@@ -24,7 +24,7 @@ while True:  # Start an infinite loop
                 while True:  # Loop to receive and write the file data
                     data = clientSocket.recv(104857600)  # Receive data of specified length
                     if not data:  # If no more data is received, break the loop
-                        break
+                        break #Break if its not a data
                     f.write(data)  # Write the received data to the file
             print('File saved to', filename)  # Print a message indicating successful file retrieval
         clientSocket.close()  # Close the client socket after retrieval
@@ -37,8 +37,7 @@ while True:  # Start an infinite loop
                 outputdata = f.read()  # Read the content of the file
             dataLength = str(len(outputdata))  # Convert the length of the output data to a string
             if int(dataLength) > 100 * 1024 * 1024:  # Check if the file size exceeds 100MB
-
-                raise IOError("File size exceeds the limit")
+                raise IOError("File size exceeds the limit") #Raising to IO Error because the file size exceeds the limit
             message = f"POST {filename} HTTP/1.1\r\nHost: {serverName}\r\n\r\n"  # Create the HTTP POST request
             clientSocket.send(message.encode())  # Send the request to the server
             clientSocket.send("HTTP/1.1 200 OK\r\n".encode())  # Send an OK response to the server
@@ -46,8 +45,8 @@ while True:  # Start an infinite loop
             clientSocket.sendall(outputdata)  # Send the file data to the server
             clientSocket.close()  # Close the client socket after sending
         except IOError:  # If the file is not found or too big
-            print("\nThe file size is too big, please choose smaller file!")
-            errorMessage = f"HTTP/1.1 404 Not Found\r\nFile not found or exceeds size limit\r\n"
+            print("\nThe file size is too big, please choose smaller file!") #Inform the user if the file size is too big
+            errorMessage = f"HTTP/1.1 404 Not Found\r\nFile not found or exceeds size limit\r\n" #Send HTTP error Message
             clientSocket.send(errorMessage.encode())  # Send a 404 Not Found response to the client
             clientSocket.close()  # Close the connection socket
 
